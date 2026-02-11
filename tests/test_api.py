32873f5dab9f9a4547322b99649a8219f0763ce7
Sunnycse10 <sunnycse10@gmail.com>
2026-02-11 14:37:46 +0200

    modified test

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,20 +18,20 @@ def test_delivery_order_price():
     assert "total_price" in data
     assert "delivery" in data
 
-# def test_missing_query_parameters():
-#     response = client.get("/api/v1/delivery-order-price")
-#     #should return 422 due to missing query parameters
-#     assert response.status_code == 422 
-
-def test_invalid_cart_value():
-    response = client.get(
-        "/api/v1/delivery-order-price",
-        params={
-            "venue_slug": "home-assignment-venue-helsinki",
-            "cart_value": 2000,
-            "user_lat": 60.17194,
-            "user_lon": 24.93087
-        }
-    )
-    #should return 422 due to invalid cart value
+def test_missing_query_parameters():
+    response = client.get("/api/v1/delivery-order-price")
+    #should return 422 due to missing query parameters
     assert response.status_code == 422 
+
+# def test_invalid_cart_value():
+#     response = client.get(
+#         "/api/v1/delivery-order-price",
+#         params={
+#             "venue_slug": "home-assignment-venue-helsinki",
+#             "cart_value": 2000,
+#             "user_lat": 60.17194,
+#             "user_lon": 24.93087
+#         }
+#     )
+#     #should return 422 due to invalid cart value
+#     assert response.status_code == 422 
